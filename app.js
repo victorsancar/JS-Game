0a53ba5983eb2096556126825b2650589f556412
victor <147457072+victorsancar@users.noreply.github.com>
2025-02-15 17:22:55 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
 function exibirTextoTela(tag, texto) {
     let campo = document.querySelector(tag);
     campo.innerHTML = texto;
+    if ('speechSynthesis' in window) {
+        let utterance = new SpeechSynthesisUtterance(texto);
+        utterance.lang = 'pt-BR'; 
+        utterance.rate = 1.2; 
+        window.speechSynthesis.speak(utterance); 
+    } else {
+        console.log("Web Speech API não suportada neste navegador.");
+    }
+
 }
 
 function exibirMensagemInicial(){
@@ -39,15 +50,28 @@ function verificarChute() {
 
 }
 
-function gerarNumeroAleatorio(){
-    return parseInt(Math.random() * 10 + 1);
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let quantidadeDeElementosNaLista = listaDeNumerosSorteados.length;
+    if (quantidadeDeElementosNaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
 
+    if (listaDeNumerosSorteados.includes(numeroEscolhido)) {
+        return gerarNumeroAleatorio();
+    }
+    else {
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        console.log(listaDeNumerosSorteados);
+        return numeroEscolhido;
+    }
 }
 
 function limpaCampo() {
     chute = document.querySelector('input');
     chute.value = '';
 }
+
 function reiniciarJogo(){
     numeroSecreto = gerarNumeroAleatorio();
     limpaCampo();
